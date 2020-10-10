f906f1a72007ee0f39e6e7d8e70e7ed73d7b6b56
md.ashrafulislam <ksashraful94@gmail.com>
2020-10-10 14:28:52 +0600

    first commit

---
+++ insertremove.js
@@ -0,0 +1,69 @@
+//insert and remove element
+
+// let arr=[3,5,6,2,9,1]
+
+//insert
+
+// arr[3]=8
+// arr.push(8)
+// arr.unshift(8)
+// arr.splice(3,0,8)
+
+//remove
+
+// arr[3]=null/undefine
+// arr.pop()
+// arr.shift()
+// arr.splice(3,1)
+// arr.splice(3,1,8)
+
+// console.log(arr)
+
+/* ```jsx
+var mainPassword = "abcd";
+var count= 1;
+
+while(1) {
+	var password = propt("input your password");
+	
+	if(password === mainPassword) {
+		//login successful
+		break;
+	} else {
+		//invalid password
+	}
+	count++;
+
+	if(count === 10) {
+		break;
+	}
+	
+}
+
+if(count === 10) {
+	//block this person
+} else {
+ 
+}
+
+//dashboard
+``` */
+
+
+/* let currentScore;
+let highScore;
+let count = 0;
+
+function calculate() {
+ if(count === 0) {
+	highScore = 0;
+ }
+ if(currentScore > highScore) {
+    highScore = currentScore;
+} 
+	count++;
+
+}
+
+calculate(7); */
+
